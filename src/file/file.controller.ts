677eda203d3057f64996db40b7ef7d1c428a8f5f
unknown <rofakvkm@gmail.com>
2021-04-23 17:20:17 +0700

    Change port

--- src/file/file.controller.ts
+++ src/file/file.controller.ts
@@ -10,7 +10,7 @@ import { Request, Response } from 'express';
 @ApiTags('file')
 export class FileController {
 
-  SERVER_URL = 'http://localhost:3000/';
+  SERVER_URL = 'http://localhost:3001/';
 
   constructor() {
   }
